129140289bcd9440f8c2ab674104592067bc5707
Joey Chen <jay.hatelove@gmail.com>
2019-06-29 14:07:08 +0800

    pass test: is valid

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using DependencyInjectionWorkshop.Models;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace DependencyInjectionWorkshopTests
@@ -9,7 +10,29 @@ namespace DependencyInjectionWorkshopTests
         [Test]
         public void is_valid()
         {
-            var authenticationService = new AuthenticationService();
+            var profile = Substitute.For<IProfile>();
+            var hash = Substitute.For<IHash>();
+            var otpService = Substitute.For<IOtpService>();
+            var failedCounter = Substitute.For<IFailedCounter>();
+            var notification = Substitute.For<INotification>();
+            var logger = Substitute.For<ILogger>();
+
+            var authenticationService = new AuthenticationService(
+                logger,
+                profile,
+                notification,
+                hash,
+                failedCounter,
+                otpService);
+
+            profile.GetPassword("joey").ReturnsForAnyArgs("abc");
+            hash.Compute("9487").ReturnsForAnyArgs("abc");
+
+            otpService.GetCurrentOtp("joey").ReturnsForAnyArgs("9527");
+
+            var isValid = authenticationService.Verify("joey", "9487", "9527");
+
+            Assert.IsTrue(isValid);
         }
     }
 }
