44badc64e5a5380f90820a65ab066ea1a8a5bc73
Joey Chen <jay.hatelove@gmail.com>
2019-06-30 09:52:14 +0800

    extract abstract class: BaseAuthenticationDecorator

--- DependencyInjectionWorkshop/Models/FailedCounterDecorator.cs
+++ DependencyInjectionWorkshop/Models/FailedCounterDecorator.cs
@@ -1,20 +1,19 @@
 ﻿namespace DependencyInjectionWorkshop.Models
 {
-    public class FailedCounterDecorator : IAuthentication
+    public class FailedCounterDecorator : BaseAuthenticationDecorator
     {
-        private readonly IAuthentication _authentication;
         private readonly IFailedCounter _failedCounter;
 
-        public FailedCounterDecorator(IFailedCounter failedCounter, IAuthentication authentication)
+        public FailedCounterDecorator(IFailedCounter failedCounter, IAuthentication authentication) : base(
+            authentication)
         {
             _failedCounter = failedCounter;
-            _authentication = authentication;
         }
 
-        public bool Verify(string accountId, string password, string otp)
+        public override bool Verify(string accountId, string password, string otp)
         {
             CheckAccountIsLocked(accountId);
-            return _authentication.Verify(accountId, password, otp);
+            return base.Verify(accountId, password, otp);
         }
 
         private void CheckAccountIsLocked(string accountId)
