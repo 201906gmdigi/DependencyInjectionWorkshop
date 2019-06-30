0bf9fab4e160ad223816b7ebe818d3bfe695fbfa
Joey Chen <jay.hatelove@gmail.com>
2019-06-30 11:42:52 +0800

    new AuthenticationService manually

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DependencyInjectionWorkshop.Models;
 
 namespace MyConsole
 {
@@ -10,6 +11,15 @@ namespace MyConsole
     {
         static void Main(string[] args)
         {
+            var notificationDecorator = new NotificationDecorator(new AuthenticationService(
+                                                                      new ProfileDao(),
+                                                                      new Sha256Adapter(),
+                                                                      new OtpService()),new SlackAdapter());
+            var failedCounterDecorator = new FailedCounterDecorator(new FailedCounter(), notificationDecorator);
+            var logFailedCountDecorator = new LogFailedCountDecorator(failedCounterDecorator, new FailedCounter(),new NLogAdapter());
+
+            var isValid = logFailedCountDecorator.Verify("joey","9487","9527");
+            Console.WriteLine(isValid);
         }
     }
 }
